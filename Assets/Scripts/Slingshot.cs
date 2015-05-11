33188b75bfd0a0525c459c088033ba8e70184828
Viktoria Masch <viktoria.masch@stud.h-da.de>
2015-05-11 15:58:35 +0200

    Created SlingShot and first script

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	void Start(){
+	
+	
+	}
+
+
+
+
+	public GameObject launchPoint;
+
+	void Awake(){
+		Transform launchPointTransform = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTransform.gameObject;
+		launchPoint.SetActive (false);
+
+	}
+
+	
+	void OnMouseEnter() {
+		launchPoint.SetActive (true);
+	
+	}
+	
+	void OnMouseExit() {
+		launchPoint.SetActive (false);
+
+
+	}
+
+
+
+
+}
