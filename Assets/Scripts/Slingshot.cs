974ae815165689af00c4ffa49369a1f358c84487
Viktoria Masch <viktoria.masch@stud.h-da.de>
2015-06-01 15:57:25 +0200

    Continued SlingShot : It's possible to drag the Prefab-Ball in the Game.

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,20 +3,24 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
 
-	void Start(){
+
+	//Fields seen in the Inspector panek
+	public GameObject prefabProjectile;
+
+	// Internal Variable
+	private GameObject    launchPoint;
+	private Vector3       launchPos;
+	private GameObject    projectile;
+
+	bool aimingMode;
+
 	
-	
-	}
-
-
-
-
-	public GameObject launchPoint;
-
 	void Awake(){
 		Transform launchPointTransform = transform.Find ("LaunchPoint");
 		launchPoint = launchPointTransform.gameObject;
 		launchPoint.SetActive (false);
+
+		launchPos = launchPointTransform.position;
 
 	}
 
@@ -31,6 +35,58 @@ public class Slingshot : MonoBehaviour {
 
 
 	}
+
+
+	void OnMouseDown() {
+
+		aimingMode = true;
+
+		// Instatiate a new Projectile
+		projectile = Instantiate (prefabProjectile) as GameObject;
+
+
+		//Start at launchPoint
+		//set the projectile's Position to the LaunchPos
+		projectile.transform.position = launchPos;
+
+		//Set isKinematic is true for now
+		projectile.GetComponent<Rigidbody> ().isKinematic = true;
+	
+
+
+	}
+
+	void Update() {
+	    //if we're not in aming mode, do nothing
+		if(!aimingMode) {
+			return;
+
+		}
+
+
+	// Get the current mouse position in 3D
+		Vector3 mousePos2D = Input.mousePosition;
+
+
+	//Convert it to 3D world coordinates
+		mousePos2D.z = - Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+	
+
+
+    //Find the difference between launchPos and mouse position
+		Vector3 mouseDelta = mousePos3D - launchPos;
+
+	
+
+    //Move the projectile to this new position
+		projectile.transform.position = mousePos3D;
+	
+	}
+
+
+
+
 
 
 
